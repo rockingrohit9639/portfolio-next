f4777e5793e773450e4754cc465e8bb6b4562668
Rohit Saini <rohitbhatwara1@gmail.com>
2025-08-28 20:41:20 +0200

    feat: enable react compiler

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from 'next';
 
 import '~/lib/env';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  experimental: {
+    reactCompiler: true,
+  },
+};
 
 export default withPayload(nextConfig);
